6a1538a3bfae123571c40f081c330669a1314f69
quratulAin <quratulain3248@gmail.com>
2024-09-17 18:15:23 +0500

    testing

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -2,6 +2,7 @@ import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 const AuthLayout = () => {
+  //test
   return (
     <>
       <Stack>
